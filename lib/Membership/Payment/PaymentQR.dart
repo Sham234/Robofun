c530d6a2e09e5da1f4dad5660a3c22e1c4cd3c07
Jason Yeo <yeo0606.jy@student.usm.my>
2023-09-19 14:46:27 +0800

    payment qr part

--- lib/Membership/Payment/PaymentQR.dart
+++ lib/Membership/Payment/PaymentQR.dart
@@ -5,9 +5,9 @@ class Paymentqr extends StatelessWidget {
   void navigateNextPage(BuildContext ctx) {
     Navigator.of(ctx).push(MaterialPageRoute(builder: (_) {
       return Payment2();
-  }));
+    }));
   }
-
+  
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -23,16 +23,16 @@ class Paymentqr extends StatelessWidget {
               colors: [Color(0x7F9912F2), Color(0xFFBD41F7)],
             ),
             shape: RoundedRectangleBorder(
-              side: BorderSide(width: 0.50, color: Colors.white),
+              side: BorderSide(width: 1, color: Colors.white),
             ),
           ),
           child: Stack(
             children: [
               Positioned(
-                left: 182,
-                top: 181,
+                left: 431,
+                top: 165,
                 child: Container(
-                  width: 1187,
+                  width: 687,
                   height: 534,
                   decoration: ShapeDecoration(
                     color: Color(0x99FEFEFE),
@@ -50,54 +50,6 @@ class Paymentqr extends StatelessWidget {
                   ),
                 ),
               ),
-              Positioned(
-                left: 300,
-                top: 223,
-                child: Container(
-                  width: 393,
-                  height: 393,
-                  decoration: ShapeDecoration(
-                    image: DecorationImage(
-                      image: NetworkImage("https://via.placeholder.com/393x393"),
-                      fit: BoxFit.cover,
-                    ),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(25),
-                    ),
-                  ),
-                ),
-              ),
-              Positioned(
-                left: 858,
-                top: 223,
-                child: Container(
-                  width: 393,
-                  height: 393,
-                  decoration: ShapeDecoration(
-                    image: DecorationImage(
-                      image: NetworkImage("https://via.placeholder.com/393x393"),
-                      fit: BoxFit.fill,
-                    ),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(25),
-                    ),
-                  ),
-                ),
-              ),
-              Positioned(
-                left: 55,
-                top: 47,
-                child: Text(
-                  'Please scan the QR code below to pay:',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 48,
-                    fontFamily: 'Inter',
-                    fontWeight: FontWeight.w400,
-                  ),
-                ),
-              ),
-              // Back button
               Positioned(
                 left: 649, 
                 top: 723,  
@@ -127,6 +79,20 @@ class Paymentqr extends StatelessWidget {
                 ),
               ),
             ),
+            Positioned(
+                left: 55,
+                top: 47,
+                child: Text(
+                  'Please show the QR Code to the camera:',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 48,
+                    fontFamily: 'Inter',
+                    fontWeight: FontWeight.w400,
+                    height: 0,
+                  ),
+                ),
+              ),
               Positioned(
                 left: 733,
                 top: 756,
@@ -137,32 +103,229 @@ class Paymentqr extends StatelessWidget {
                     fontSize: 36,
                     fontFamily: 'Inter',
                     fontWeight: FontWeight.w400,
+                    height: 0,
                   ),
                 ),
               ),
               Positioned(
-                left: 333,
-                top: 638,
-                child: Text(
-                  'Touch n Go eWallet',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 36,
-                    fontFamily: 'Inter',
-                    fontWeight: FontWeight.w400,
+                left: 686,
+                top: 357,
+                child: Container(
+                  width: 177,
+                  height: 150,
+                  decoration: ShapeDecoration(
+                    color: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    shadows: [
+                      BoxShadow(
+                        color: Color(0xFF3197FD),
+                        blurRadius: 4,
+                        offset: Offset(0, 4),
+                        spreadRadius: 0,
+                      )
+                    ],
                   ),
                 ),
               ),
               Positioned(
-                left: 939,
-                top: 638,
-                child: Text(
-                  'MAE eWallet',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 36,
-                    fontFamily: 'Inter',
-                    fontWeight: FontWeight.w400,
+                left: 715,
+                top: 372,
+                child: Container(
+                  width: 120,
+                  height: 120,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: NetworkImage("https://via.placeholder.com/120x120"),
+                      fit: BoxFit.contain,
+                    ),
+                  ),
+                ),
+              ),
+              Positioned(
+                left: 551,
+                top: 209,
+                child: Container(
+                  width: 44.70,
+                  height: 44.70,
+                  child: Stack(
+                    children: [
+                      Positioned(
+                        left: -0,
+                        top: 0.89,
+                        child: Container(
+                          width: 44.70,
+                          decoration: ShapeDecoration(
+                            shape: RoundedRectangleBorder(
+                              side: BorderSide(
+                                width: 1,
+                                strokeAlign: BorderSide.strokeAlignCenter,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        left: 0.89,
+                        top: 44.70,
+                        child: Transform(
+                          transform: Matrix4.identity()..translate(0.0, 0.0)..rotateZ(-1.57),
+                          child: Container(
+                            width: 44.70,
+                            decoration: ShapeDecoration(
+                              shape: RoundedRectangleBorder(
+                                side: BorderSide(
+                                  width: 1,
+                                  strokeAlign: BorderSide.strokeAlignCenter,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Positioned(
+                left: 953.30,
+                top: 209,
+                child: Container(
+                  width: 44.70,
+                  height: 44.70,
+                  child: Stack(
+                    children: [
+                      Positioned(
+                        left: 43.81,
+                        top: 0,
+                        child: Transform(
+                          transform: Matrix4.identity()..translate(0.0, 0.0)..rotateZ(1.57),
+                          child: Container(
+                            width: 44.70,
+                            decoration: ShapeDecoration(
+                              shape: RoundedRectangleBorder(
+                                side: BorderSide(
+                                  width: 1,
+                                  strokeAlign: BorderSide.strokeAlignCenter,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        left: 0,
+                        top: 0.89,
+                        child: Container(
+                          width: 44.70,
+                          decoration: ShapeDecoration(
+                            shape: RoundedRectangleBorder(
+                              side: BorderSide(
+                                width: 1,
+                                strokeAlign: BorderSide.strokeAlignCenter,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Positioned(
+                left: 953.30,
+                top: 611.30,
+                child: Container(
+                  width: 44.70,
+                  height: 44.70,
+                  child: Stack(
+                    children: [
+                      Positioned(
+                        left: 44.70,
+                        top: 43.81,
+                        child: Transform(
+                          transform: Matrix4.identity()..translate(0.0, 0.0)..rotateZ(3.14),
+                          child: Container(
+                            width: 44.70,
+                            decoration: ShapeDecoration(
+                              shape: RoundedRectangleBorder(
+                                side: BorderSide(
+                                  width: 1,
+                                  strokeAlign: BorderSide.strokeAlignCenter,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        left: 43.81,
+                        top: 0,
+                        child: Transform(
+                          transform: Matrix4.identity()..translate(0.0, 0.0)..rotateZ(1.57),
+                          child: Container(
+                            width: 44.70,
+                            decoration: ShapeDecoration(
+                              shape: RoundedRectangleBorder(
+                                side: BorderSide(
+                                  width: 1,
+                                  strokeAlign: BorderSide.strokeAlignCenter,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Positioned(
+                left: 551,
+                top: 611.30,
+                child: Container(
+                  width: 44.70,
+                  height: 44.70,
+                  child: Stack(
+                    children: [
+                      Positioned(
+                        left: 0.89,
+                        top: 44.70,
+                        child: Transform(
+                          transform: Matrix4.identity()..translate(0.0, 0.0)..rotateZ(-1.57),
+                          child: Container(
+                            width: 44.70,
+                            decoration: ShapeDecoration(
+                              shape: RoundedRectangleBorder(
+                                side: BorderSide(
+                                  width: 1,
+                                  strokeAlign: BorderSide.strokeAlignCenter,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        left: 44.70,
+                        top: 43.81,
+                        child: Transform(
+                          transform: Matrix4.identity()..translate(0.0, 0.0)..rotateZ(-3.14),
+                          child: Container(
+                            width: 44.70,
+                            decoration: ShapeDecoration(
+                              shape: RoundedRectangleBorder(
+                                side: BorderSide(
+                                  width: 1,
+                                  strokeAlign: BorderSide.strokeAlignCenter,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
               ),
